1c7ebf28ed4c10c40954004a7e96472fcbf647e8
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-06-26 14:41:00 +0300

    Corrections from TA

--- src/main.js
+++ src/main.js
@@ -10,15 +10,15 @@ let initialState = {
 
 const clickEdit = inputText => {
   return {
-	type: 'click_edit',
-	inputText
+    type: 'click_edit',
+    inputText
   }
 };
 
 const clickEnter = inputText => {
   return {
-	type: 'click_enter',
-	inputText
+    type: 'click_enter',
+    inputText
   }
 };
 
@@ -30,54 +30,54 @@ const removeItem = () => {
 
 const exitFromInput = () => {
   return {
-	type: 'exit_from_input'
+	  type: 'exit_from_input'
   }
 };
 
 function getNextState(state = initialState, action) {
   switch (action.type) {
-	case 'click_edit':
-	  return {
-		...state,
-		index: state.items.indexOf(action.inputText),
-	  inputText: action.inputText
-	};
-	case 'click_enter':
-	  return {
-		...state,
-		items: state.items.map(item => {
-		  return state.items.indexOf(item) === state.index
-			? action.inputText
-			: item;
-		}),
-		index: null,
-		inputText: ''
-	  };
-	case 'remove_item':
-	  return {
-		...state,
-		items: state.items.filter(item => {
-		  return state.items.indexOf(item) !== state.index;
-		}),
-		index: null,
-		inputText: ''
-	  }
-	case 'exit_from_input':
-	  return {
-		...state,
-		index: null,
-		inputText: null
-	  }
-	default:
-	  return state;
-  }
+    case 'click_edit':
+      return {
+        ...state,
+        index: state.items.indexOf(action.inputText),
+        inputText: action.inputText
+      };
+    case 'click_enter':
+        return {
+          ...state,
+          items: state.items.map(item => {
+            return state.items.indexOf(item) === state.index
+                  ? action.inputText
+                  : item;
+          }),
+          index: null,
+          inputText: ''
+        };
+    case 'remove_item':
+      return {
+        ...state,
+        items: state.items.filter(item => {
+          return state.items.indexOf(item) !== state.index;
+        }),
+        index: null,
+        inputText: ''
+      }
+    case 'exit_from_input':
+      return {
+        ...state,
+        index: null,
+        inputText: null
+      }
+    default:
+      return state;
+    }
 }
 
 const store = Redux.createStore(getNextState);
 
 document.addEventListener('click', (event) => {
   if (!event.target.closest('button') && !event.target.closest('input')) {
-	store.dispatch(exitFromInput());
+	  store.dispatch(exitFromInput());
   }
 })
 
@@ -85,37 +85,37 @@ function render() {
   container.innerHTML = '';
 
   store.getState().items.forEach(item => {
-	const li = document.createElement('li');
-	li.textContent = item;
-	
-	if (store.getState().inputText !== item) {
-	  const button = document.createElement('button');
-	  button.textContent = 'Edit';
-	  li.append(button);
+    const li = document.createElement('li');
+    li.textContent = item;
 
-	button.addEventListener('click', () => {
-	  store.dispatch(clickEdit(item));
-	});
-	} else {
-	  const input = document.createElement('input');
-	  input.value = item;
-	  input.autofocus = true;
-	  li.append(input);
+    if (store.getState().inputText !== item) {
+      const button = document.createElement('button');
+      button.textContent = 'Edit';
+      li.append(button);
 
-	  input.addEventListener('keydown', event => {
-		if (event.key !== 'Enter') {
-		  return;
-		};
-		
-		if (event.target.value.trim() === '') {
-		  store.dispatch(removeItem());
-		}
+      button.addEventListener('click', () => {
+        store.dispatch(clickEdit(item));
+      });
+    } else {
+      const input = document.createElement('input');
+      input.value = item;
+      input.autofocus = true;
+      li.append(input);
 
-		store.dispatch(clickEnter(event.target.value.trim()));
-	  });
-	}
-	
-	container.append(li);
+      input.addEventListener('keydown', event => {
+        if (event.key !== 'Enter') {
+          return;
+        };
+
+        if (event.target.value.trim() === '') {
+          store.dispatch(removeItem());
+        }
+
+        store.dispatch(clickEnter(event.target.value.trim()));
+      });
+    }
+
+    container.append(li);
   });
 }
 
